68c6be204369e1d0ddb21cdf46698280fb4ea50b
NemoNotFound <unconfigured@null.spigotmc.org>
2025-04-03 16:49:49 +0200

    Update mixin

--- common/src/main/java/com/nemonotfound/nemos/copper/mixin/CauldronInteractionMixin.java
+++ common/src/main/java/com/nemonotfound/nemos/copper/mixin/CauldronInteractionMixin.java
@@ -5,21 +5,30 @@ import com.llamalad7.mixinextras.sugar.Local;
 import com.nemonotfound.nemos.copper.block.CopperCauldronBlock;
 import com.nemonotfound.nemos.copper.block.LayeredCopperCauldronBlock;
 import com.nemonotfound.nemos.copper.block.ModBlocks;
+import com.nemonotfound.nemos.copper.item.Items;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.cauldron.CauldronInteraction;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.LayeredCauldronBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.level.material.FluidState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.minecraft.core.cauldron.CauldronInteraction.emptyBucket;
 
@@ -86,7 +95,22 @@ public interface CauldronInteractionMixin {
         return original;
     }
 
-    //TODO: Inject into emptyBucket()
+    @Inject(method = "emptyBucket", at = @At("HEAD"), cancellable = true)
+    private static void emptyCopperBucket(Level level, BlockPos pos, Player player, InteractionHand hand, ItemStack filledStack, BlockState state, SoundEvent emptySound, CallbackInfoReturnable<InteractionResult> cir) {
+        var bucketItem = player.getItemInHand(hand);
+
+        if (bucketItem.is(Items.COPPER_BUCKET.get()) && !level.isClientSide) {
+            Item item = filledStack.getItem();
+            player.setItemInHand(hand, ItemUtils.createFilledResult(filledStack, player, new ItemStack(Items.COPPER_BUCKET.get())));
+            player.awardStat(Stats.FILL_CAULDRON);
+            player.awardStat(Stats.ITEM_USED.get(item));
+            level.setBlockAndUpdate(pos, state);
+            level.playSound(null, pos, emptySound, SoundSource.BLOCKS, 1.0F, 1.0F);
+            level.gameEvent(null, GameEvent.FLUID_PLACE, pos);
+
+            cir.setReturnValue(InteractionResult.SUCCESS);
+        }
+    }
 
     //TODO: fillBucket
 
